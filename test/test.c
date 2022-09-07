e55ac1e5f2216c506d2b47cd768f6fcbecf4c063
Kevin Albertson <kevin.eric.albertson@gmail.com>
2022-09-06 20:19:58 -0400

    add test of two bytes

--- test/test.c
+++ test/test.c
@@ -45,35 +45,52 @@ int main() {
   test_hexstring_to_bytes();
   test_bitstring_to_bytes();
 
-  /* Test encoding one byte */
-  {
-    size_t in_len;
-    byte *in = bitstring_to_bytes("11111111", &in_len);
-    byte got[2] = {0};
-    size_t got_len;
-    base122_error_t error;
-    size_t expect_len;
-    byte *expect = bitstring_to_bytes("01111111 01000000", &expect_len);
-    int ret = base122_encode(in, in_len, got, sizeof(got), &got_len, &error);
-    ASSERT(ret != -1, "base122_encode error: %s", error.msg);
-    ASSERT_BYTES_EQUAL(in, in_len, got, got_len, bitstring);
-    free(expect);
-    free(in);
-  }
+  typedef struct {
+    const char *description;
+    const char *data;
+    const char *encoded;
+  } roundtrip_test_t;
 
-  /* Test decoding two bytes */
-  {
-    size_t in_len;
-    byte *in = bitstring_to_bytes("01111111 01000000", &in_len);
-    byte got[1] = {0};
-    size_t got_len;
-    base122_error_t error;
-    size_t expect_len;
-    byte *expect = bitstring_to_bytes("11111111", &expect_len);
-    int ret = base122_decode(in, in_len, got, sizeof(got), &got_len, &error);
-    ASSERT(ret != -1, "base122_encode error: %s", error.msg);
-    ASSERT_BYTES_EQUAL(in, in_len, got, got_len, bitstring);
-    free(expect);
-    free(in);
+  roundtrip_test_t tests[] = {
+      {.description = "one byte", .data = "11111111", .encoded = "01111111 01000000"},
+      {.description = "several bytes",
+       .data = "10101010 10101010 10101010 10101010",
+       .encoded = "01010101 00101010 01010101 00101010 01010000"}};
+
+  for (size_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+    roundtrip_test_t *test = tests + i;
+
+    printf("test '%s'\n", test->description);
+    /* Test encoding 'data'. */
+    {
+      size_t in_len;
+      byte *in = bitstring_to_bytes(test->data, &in_len);
+      byte *got = malloc(sizeof(byte) * in_len);
+      size_t got_len;
+      base122_error_t error;
+      size_t expect_len;
+      byte *expect = bitstring_to_bytes(test->encoded, &expect_len);
+      int ret = base122_encode(in, in_len, got, sizeof(got), &got_len, &error);
+      ASSERT(ret != -1, "base122_encode error: %s", error.msg);
+      ASSERT_BYTES_EQUAL(expect, expect_len, got, got_len, bitstring);
+      free(expect);
+      free(in);
+    }
+
+    /* Test decoding 'encoded'. */
+    {
+      size_t in_len;
+      byte *in = bitstring_to_bytes(test->encoded, &in_len);
+      byte got[1] = {0};
+      size_t got_len;
+      base122_error_t error;
+      size_t expect_len;
+      byte *expect = bitstring_to_bytes(test->data, &expect_len);
+      int ret = base122_decode(in, in_len, got, sizeof(got), &got_len, &error);
+      ASSERT(ret != -1, "base122_decode error: %s", error.msg);
+      ASSERT_BYTES_EQUAL(expect, expect_len, got, got_len, bitstring);
+      free(expect);
+      free(in);
+    }
   }
 }
