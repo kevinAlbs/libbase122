5e57c7e79a569305da5890fb285dad4de05b6a27
Kevin Albertson <kevin.eric.albertson@gmail.com>
2022-09-09 06:07:12 -0400

    add stub impl

--- src/libbase122.c
+++ src/libbase122.c
@@ -2,7 +2,9 @@
 
 int base122_encode(const unsigned char *in, size_t in_len, unsigned char *out, size_t out_len,
                    size_t *out_written, base122_error_t *error) {
-  return -1;
+  out[0] = 0;
+  *out_written = 1;
+  return 0;
 }
 
 int base122_decode(const unsigned char *in, size_t in_len, unsigned char *out, size_t out_len,
--- src/libbase122.h
+++ src/libbase122.h
@@ -10,13 +10,13 @@ typedef struct {
 
 /* Encodes data in base122.
  * Pass NULL for `out` to determine the exact size required for `out`.
- * Returns false on error. */
+ * Returns -1 on error. */
 int base122_encode(const unsigned char *in, size_t in_len, unsigned char *out, size_t out_len,
                    size_t *out_written, base122_error_t *error);
 
 /* Decodes data in base122.
  * Pass NULL for `out` to determine the exact size required for `out`.
- * Returns false on error. */
+ * Returns -1 on error. */
 int base122_decode(const unsigned char *in, size_t in_len, unsigned char *out, size_t out_len,
                    size_t *out_written, base122_error_t *error);
 
