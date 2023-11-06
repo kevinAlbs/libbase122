d49d55b4266a3c710bec1f6602d33b61cd9be824
Kevin Albertson <kevin.eric.albertson@gmail.com>
2023-11-06 10:11:40 -0500

    add BASE122_EXPORT macro

--- src/base122.h
+++ src/base122.h
@@ -7,16 +7,31 @@ typedef struct {
   char msg[254];
 } base122_error_t;
 
+/* Define export macro. */
+#define BASE122_EXPORT
+
+#if defined(_MSC_VER) && !defined(BASE122_STATIC) && !defined(BASE122_COMPILING)
+/* Using shared library. Import symbols. */
+#undef BASE122_EXPORT
+#define BASE122_EXPORT __declspec(dllimport)
+#endif
+
+#if defined(_MSC_VER) && !defined(BASE122_STATIC) && defined(BASE122_COMPILING)
+/* Building shared library. Export symbols. */
+#undef BASE122_EXPORT
+#define BASE122_EXPORT __declspec(dllexport)
+#endif
+
 /* Encodes data in base122.
  * Pass NULL for `out` to determine the exact size required for `out`.
  * Returns -1 on error. */
-int base122_encode(const unsigned char *in, size_t in_len, unsigned char *out, size_t out_len,
-                   size_t *out_written, base122_error_t *error);
+int BASE122_EXPORT base122_encode(const unsigned char *in, size_t in_len, unsigned char *out,
+                                  size_t out_len, size_t *out_written, base122_error_t *error);
 
 /* Decodes data in base122.
  * Pass NULL for `out` to determine the exact size required for `out`.
  * Returns -1 on error. */
-int base122_decode(const unsigned char *in, size_t in_len, unsigned char *out, size_t out_len,
-                   size_t *out_written, base122_error_t *error);
+int BASE122_EXPORT base122_decode(const unsigned char *in, size_t in_len, unsigned char *out,
+                                  size_t out_len, size_t *out_written, base122_error_t *error);
 
 #endif /* LIBBASE122_H */
